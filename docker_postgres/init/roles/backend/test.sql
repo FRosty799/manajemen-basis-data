1aacc8d13fdf96f8a84d24b7135fb20eaa76cd45
FRosty799 <fauzanramdhani7@gmail.com>
2025-10-26 20:49:00 +0700

    testsql

---
+++ docker_postgres/init/roles/backend/test.sql
@@ -0,0 +1,3 @@
+INSERT INTO SALAM.mahasiswas VALUES ('231010', 'Test CRUD', 'test@example.com', 22);
+UPDATE SALAM.mahasiswas SET nama = 'Update OK' WHERE nim = '231010';
+DELETE FROM SALAM.mahasiswas WHERE nim = '231010';
---
+++ docker_postgres/init/roles/bi_dev/test.sql
@@ -0,0 +1,2 @@
+SELECT * FROM SALAM.mahasiswas;
+INSERT INTO SALAM.mahasiswas VALUES ('231011', 'Coba', 'coba@example.com', 22);
